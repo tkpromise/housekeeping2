7e221ee406eba6961433e9dc6d1a945bfd397a2c
tkpromise <tkpromise@foxmail.com>
2019-03-08 22:49:05 +0800

    first commit

---
+++ cloudfunctions/roomstatus/index.js
@@ -0,0 +1,32 @@
+// 云函数入口文件
+const cloud = require('wx-server-sdk')
+const got = require("got")
+
+cloud.init()
+
+// 云函数入口函数
+exports.main = async (event, context) => {
+  let xml = '' +
+    '<?xml version="1.0" encoding="utf-8"?>' +
+    '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+    '<soap:Body>' +
+    '<GetCurrentRoomStatusGraph xmlns="www.mingyansoft.com">' +
+    '<nChainID>1</nChainID>' +
+    '<nRoomTypeID>1</nRoomTypeID>' +
+    '<lsRoomNo>' +
+    '<string>8205</string>' +
+    '<string>8209</string>' +
+    '</lsRoomNo>' +
+    '</GetCurrentRoomStatusGraph>' +
+    '</soap:Body>' +
+    '</soap:Envelope>'
+  let postResponse = await got('http://114.55.172.147:9702/MobileHouseKeepingService.asmx?op=GetCurrentRoomStatusGraph', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'text/xml; charset=utf-8'
+    },
+    body: xml,
+  })
+
+  return postResponse.body
+}
---
+++ miniprogram/pages/xml2json/xml2json.js
@@ -0,0 +1,42 @@
+var xml2json = require('xml2json.min.js');
+Page ({
+    data: {
+
+    },
+    onLoad:function(options) {
+        let xml = '' +
+    '<?xml version="1.0" encoding="utf-8"?>' +
+    '<soap:Envelope xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance" xmlns:xsd="http://www.w3.org/2001/XMLSchema" xmlns:soap="http://schemas.xmlsoap.org/soap/envelope/">' +
+    '<soap:Body>' +
+    '<GetCurrentRoomStatusGraph xmlns="www.mingyansoft.com">' +
+    '<nChainID>1</nChainID>' +
+    '<nRoomTypeID>1</nRoomTypeID>' +
+    '<lsRoomNo>' +
+    '<string>8205</string>' +
+    '<string>8209</string>' +
+    '</lsRoomNo>' +
+    '</GetCurrentRoomStatusGraph>' +
+    '</soap:Body>' +
+    '</soap:Envelope>'
+        //console.log(xml2json(xml))
+        //console.log(xml)
+        wx.cloud.callFunction({
+            name: 'roomstatus',
+            complete:res => {
+              console.log(res)
+              console.log('--------')
+              let string = JSON.stringify(xml2json(res.result))
+              console.log(xml2json(res.result))
+              //console.log(string)
+              //console.log(JSON.parse(string))
+              this.setData({
+                //result:(JSON.parse(string))
+                result:xml2json(res.result)
+
+              })
+            }
+        })
+    }
+    
+})
+
